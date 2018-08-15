04f62d38571007ff54d6634f6b9c992533317452
김병철 <peppi@outlook.kr>
2018-08-16 04:27:11 +0900

    초기 커밋.

---
+++ WindowServer01/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowServer01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(26, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "서버 ip :";
+            // 
+            // ipBox
+            // 
+            this.ipBox.Location = new System.Drawing.Point(117, 8);
+            this.ipBox.Name = "ipBox";
+            this.ipBox.Size = new System.Drawing.Size(207, 25);
+            this.ipBox.TabIndex = 1;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(356, 9);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(88, 23);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "서버 시작";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(29, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(495, 259);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(26, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "연결 클라이언트 ip list";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 371);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.ipBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowServer01/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace WindowServer01
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private TcpListener tcpListener = null;
+        //private BinaryReader br = null;
+        //private BinaryWriter bw = null;
+        //private NetworkStream ns;
+
+        //int intValue;
+        //float floatValue;
+        //string strValue;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tcpListener = new TcpListener(3000);
+            tcpListener.Start();
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            for(int i = 0; i < host.AddressList.Length; i++)
+            {
+                if(host.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ipBox.Text = host.AddressList[i].ToString();
+                    break;
+                }
+            }
+        }
+
+        private void AcceptClient()
+        {
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                if (tcpClient.Connected)
+                {
+                    string str = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+                    listBox1.Items.Add(str);
+                }
+
+                EchoServer echoServer = new EchoServer(tcpClient);
+                Thread th = new Thread(new ThreadStart(echoServer.Process));
+                th.IsBackground = true;
+                th.Start();
+            }
+        }
+
+        /// <summary>
+        /// 접속 시작 버튼
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(new ThreadStart(AcceptClient));
+            th.IsBackground = true;
+            th.Start();
+
+            //tcpClient = tcpListener.AcceptTcpClient();
+            //if (tcpClient.Connected)
+            //{
+            //    countBox.Text = ((IPEndPoint)tcpClient.Client.RemoteEndPoint).Address.ToString();
+            //}
+            //ns = tcpClient.GetStream();
+            //bw = new BinaryWriter(ns);
+            //br = new BinaryReader(ns);
+        }
+
+        //private void srBtn_Click(object sender, EventArgs e)
+        //{
+        //    while (true)
+        //    {
+        //        if (tcpClient.Connected)
+        //        {
+        //            if (DataReceive() == -1)
+        //                break;
+        //            DataSend();
+        //        }
+        //        else
+        //        {
+        //            AllClose();
+        //            break;
+        //        }
+        //    }
+        //    AllClose();
+        //}
+
+        //private int DataReceive()
+        //{
+        //    intValue = br.ReadInt32();
+        //    if (intValue == -1)
+        //        return -1;
+
+        //    floatValue = br.ReadSingle();
+        //    strValue = br.ReadString();
+        //    string str = intValue + "/" + floatValue + "/" + strValue;
+        //    MessageBox.Show(str);
+        //    return 0;
+        //}
+
+        
+
+        //private void DataSend()
+        //{
+        //    bw.Write(intValue);
+        //    bw.Write(floatValue);
+        //    bw.Write(strValue);
+
+        //    MessageBox.Show("보냈습니다.");
+        //}
+
+        //private void AllClose()
+        //{
+        //    if (bw != null)
+        //    {  bw.Close(); bw = null;   }
+        //    if (br != null)
+        //    { br.Close(); br = null; }
+        //    if (ns != null)
+        //    { ns.Close(); ns = null; }
+        //    if (tcpClient != null)
+        //    { tcpClient.Close(); tcpClient = null; }
+        //}
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //AllClose();
+            //tcpListener.Stop();
+            if(tcpListener != null)
+            {
+                tcpListener.Stop();
+                tcpListener = null;
+            }
+        }
+    }
+
+    class EchoServer
+    {
+        TcpClient RefClient;
+        private BinaryReader br = null;
+        private BinaryWriter bw = null;
+        int intValue;
+        float floatValue;
+        string strValue;
+
+        public EchoServer(TcpClient Client)
+        {
+            RefClient = Client;
+        }
+
+        public void Process()
+        {
+            NetworkStream ns = RefClient.GetStream();
+            try
+            {
+                br = new BinaryReader(ns);
+                bw = new BinaryWriter(ns);
+
+                while (true)
+                {
+                    intValue = br.ReadInt32();
+                    floatValue = br.ReadSingle();
+                    strValue = br.ReadString();
+
+                    bw.Write(intValue);
+                    bw.Write(floatValue);
+                    bw.Write(strValue);
+                }
+
+            }catch(SocketException se)
+            {
+                br.Close();
+                bw.Close();
+                ns.Close();
+                ns = null;
+                RefClient.Close();
+                MessageBox.Show(se.Message);
+                Thread.CurrentThread.Abort();
+            }catch(IOException ex)
+            {
+                br.Close();
+                bw.Close();
+                ns.Close();
+                ns = null;
+                RefClient.Close();
+                MessageBox.Show(ex.Message);
+                Thread.CurrentThread.Abort();
+            }
+        }
+    }
+}
